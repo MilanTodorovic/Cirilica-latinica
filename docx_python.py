62efc16665ffd47c6574b451027fbddbde013b6c
Milan Todorovic <todor-90@hotmail.com>
2018-03-01 16:46:34 +0100

    Add files via upload
    Added some output info.

--- docx_python.py
+++ docx_python.py
@@ -1,5 +1,5 @@
 from docx import Document
-
+import sys, os, time
 
 def transliteracija(text):
     global letters_dct, digraphs_dct
@@ -19,6 +19,8 @@ def transliteracija(text):
             
     return _text
 
+t1 = time.time()
+print("Program starting...", time.strftime("%H:%M:%S", time.gmtime()))
 
 lst = []
 letters = "абвгдђежзијклмнопрстћуфхцчш"
@@ -37,10 +39,23 @@ capital_letters_dct = {x:y for x,y in zip(capital_letters,
 capital_digraphs_dct = {x:y for x,y in zip(capital_digraphs,
                                        capital_latin_digraphs)}
 
+cwd = os.getcwd()
+files = os.listdir(cwd)
+nr_files = len(list(filter(lambda x: ".docx" in x, files)))
+number = 0
 
-with open("Eskimski-cobissnet.docx", "rb") as f:
-    doc = Document(f)
-    for paragraph in doc.paragraphs:
-        for run in paragraph.runs:
-            run.text = transliteracija(run.text)
-    doc.save("experiment.docx")
+for file in files:
+    if ".docx" in file:
+        number+=1
+        sys.stdout.write("Processing file: {0} of {1}\n".format(number, nr_files))
+        sys.stdout.flush()
+        with open(file, "rb") as f:
+            doc = Document(f)
+            for paragraph in doc.paragraphs:
+                for run in paragraph.runs:
+                    run.text = transliteracija(run.text)
+            doc.save("latin_{}".format(file))
+            
+t2 = time.time()
+print("Done.", time.strftime("%H:%M:%S", time.gmtime()))
+print("Time elapsed: {0:.6f} seconds".format(t2-t1))
